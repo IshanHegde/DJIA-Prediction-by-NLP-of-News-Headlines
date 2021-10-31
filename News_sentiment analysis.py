a057c42db86f3cb2254113559a1d875d834ca8e3
Ishan Hegde <ihegde@connect.ust.hk>
2021-10-31 22:56:13 +0800

    Update News_sentiment analysis.py
    Updating Ensemble method

--- News_sentiment analysis.py
+++ News_sentiment analysis.py
@@ -10,6 +10,7 @@ import numpy as np
 import sklearn.preprocessing as preprocessing
 import matplotlib.pyplot as plt
 import nltk
+import scipy
 from keras.models import Sequential, Model
 from keras.layers import Dense, Activation, Dropout, LSTM, Input
 nltk.download('wordnet')
@@ -27,8 +28,14 @@ import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
 from sklearn.model_selection import cross_validate, GridSearchCV
 import sklearn.metrics as metrics
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
-df = pandas.read_csv(r"C:\Users\ishan\Desktop\Project\stock\Combined_News_DJIA.csv\Combined_News_DJIA.csv")
+df = pandas.read_csv(r"C:\Users\ishan\Desktop\Project\DJIA-prediction-from-news\Combined_News_DJIA.csv")
 
 df.isna().sum()
 
@@ -87,6 +94,7 @@ vec_data_test = cv.transform(lemm_test)
 y_train=df_train['Label']
 X_train=vec_data_train
 
+## XG boost
 
 param_tuning = {
         'learning_rate': [0.01, 0.1],
@@ -145,22 +153,188 @@ tn, fp, fn, tp = cm.ravel()
 recall = tp/(tp+fn)
 print(recall)
 
+# recall = 85%
+
 precision = tp/(fp+tp)
 print(precision)
+
+# precision = 83% 
 
 accuracy = accuracy_score(df_test['Label'], predictions)
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
+# accuracy = 84%
+
+## Random Forest 
 
 
+clf_rf = RandomForestClassifier().fit(X_train,y_train)
 
 
+ypred = clf_rf.predict(vec_data_test)
+
+cm = confusion_matrix(df_test['Label'], ypred)
+tn, fp, fn, tp = cm.ravel()
+(tn, fp, fn, tp)
+
+recall = tp/(tp+fn)
+print(recall)
+
+# recall = 99.5%
+
+precision = tp/(fp+tp)
+print(precision)
+
+# precision =77%
+
+accuracy = accuracy_score(df_test['Label'], ypred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+# acucracy = 85 % 
+
+## Naive Bayes
 
 
+clf = GaussianNB()
+clf.fit(X_train.toarray(),y_train)
+
+ypred = clf.predict(vec_data_test.toarray())
+
+cm = confusion_matrix(df_test['Label'], ypred)
+tn, fp, fn, tp = cm.ravel()
+(tn, fp, fn, tp)
+
+recall = tp/(tp+fn)
+print(recall)
+
+# recall = 87%
+
+precision = tp/(fp+tp)
+print(precision)
+
+# precision = 80%
+
+accuracy = accuracy_score(df_test['Label'], ypred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+# accuracy 83%
 
 
+## Multi-Layer Perceptron 
+
+clf = MLPClassifier(hidden_layer_sizes=(100,2),activation='relu',random_state=0, max_iter=300)
+
+clf.fit(X_train, y_train)
+
+ypred =clf.predict(vec_data_test.toarray())
+
+cm = confusion_matrix(df_test['Label'], ypred)
+tn, fp, fn, tp = cm.ravel()
+(tn, fp, fn, tp)
+
+recall = tp/(tp+fn)
+print(recall)
+
+#recall = 64%
+
+precision = tp/(fp+tp)
+print(precision)
+
+# precision = 100% 
+
+accuracy = accuracy_score(df_test['Label'], ypred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+# accuracy = 81.5% 
 
 
+## K-NN 
+
+neigh = KNeighborsClassifier(n_neighbors=8)
+neigh.fit(X_train, y_train)
+
+ypred =neigh.predict(vec_data_test)
+
+cm = confusion_matrix(df_test['Label'], ypred)
+tn, fp, fn, tp = cm.ravel()
+(tn, fp, fn, tp)
+
+recall = tp/(tp+fn)
+print(recall)
+
+# recall = 99%
+
+precision = tp/(fp+tp)
+print(precision)
+
+# precision = 76%
+
+accuracy = accuracy_score(df_test['Label'], ypred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+
+## Ensamble of MLP and Random Forest:
+    
+mlp_pred_proba = clf.predict_proba(vec_data_test.toarray())
+
+fr_pred_proba = clf_rf.predict_proba(vec_data_test.toarray())
+
+idx = df_test['Label'].index
+mlp = pandas.DataFrame(mlp_pred_proba,index=idx)
+rf= pandas.DataFrame(fr_pred_proba,index=idx)
+
+combined = pandas.concat([rf[0],mlp[1]],axis=1)
+
+rf2 = pandas.DataFrame(np.array([rf.iloc[i,0]>0.5 for i in range(len(rf))]).astype(int),index=idx)
+mlp2 = pandas.DataFrame(np.array([mlp.iloc[i,1]>0.5 for i in range(len(mlp))]).astype(int),index=idx)
+
+average_prob = pandas.concat([rf[0],mlp[1]*0.89],axis=1)
+
+average_prob[0].mean()
+max(average_prob[0])
+average_prob[1].mean()
+max(average_prob[1])
+
+ensamble_pred = np.array([average_prob.iloc[i,1]>average_prob.iloc[i,0] for i in range(len(average_prob)) ]).astype(int)
+
+cm = confusion_matrix(df_test['Label'],ensamble_pred)
+tn, fp, fn, tp = cm.ravel()
+(tn, fp, fn, tp)
+
+recall = tp/(tp+fn)
+print(recall)
+
+precision = tp/(fp+tp)
+print(precision)
+
+
+accuracy = accuracy_score(df_test['Label'], ensamble_pred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+eclf = VotingClassifier(estimators=[('rf',clf_rf),('mlp',clf),('KNN',neigh)],voting='soft')
+
+eclf = eclf.fit(X_train,y_train)
+
+ypred = eclf.predict(vec_data_test.toarray())
+
+cm = confusion_matrix(df_test['Label'], ypred)
+tn, fp, fn, tp = cm.ravel()
+(tn, fp, fn, tp)
+
+recall = tp/(tp+fn)
+print(recall)
+
+# recall = 80%
+
+precision = tp/(fp+tp)
+print(precision)
+
+# precision = 95%
+
+accuracy = accuracy_score(df_test['Label'], ypred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+# accuracy = 88%
 
 
 
